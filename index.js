67abe87d07d129ccc657d226d26feb45a296bde0
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2024-07-31 16:20:56 +0900

    2.4 update

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to my app!');
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
-});
-
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
-});
-
 // Define your top 10 movies data
 const top10Movies = [
     { title: 'Movie 1', year: 2022 },
@@ -31,37 +23,6 @@ const top10Movies = [
   });
   
 
-
-let myLogger = (req, res, next) => {
-  console.log(req.url);
-  next();
-};
-
-let requestTime = (req, res, next) => {
-  req.requestTime = Date.now();
-  next();
-};
-
-app.use(myLogger);
-app.use(requestTime);
-
-app.get('/', (req, res) => {
-  let responseText = 'Welcome to my app!';
-  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-  res.send(responseText);
-});
-
-app.get('/secreturl', (req, res) => {
-  let responseText = 'This is a secret url with super top-secret content.';
-  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
-  res.send(responseText);
-
-});
-
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
-});
-
 // Middleware for serving static files from the public directory
 app.use(express.static('public'));
 
@@ -71,10 +32,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-// Start the server and listen for requests on port 8080
-const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
 });
-
 
