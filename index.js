79e6c3407e60075ab4e810a8c32f2d5f5ad509fc
PeterWorld816 <113797381+PeterWorld816@users.noreply.github.com>
2024-08-01 11:35:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.get('/movies', (req, res) => {
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');});
+  res.status(500).send('Something broke!');
+});
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
